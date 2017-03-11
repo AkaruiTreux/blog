0f278b9aa12edca3537b5a72b03613f9e149b38e
Elmer Roderick Umale <akarui.treux@gmail.com>
2017-03-11 03:59:07 +0000

    Additional Refactoring

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,14 @@
 class CommentsController < ApplicationController
+  before_action :set_article
+  
   http_basic_authenticate_with name: "akarui", password: "secret", only: :destroy
   
   def create
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
   
   def destroy
-    @article = Article.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article)
@@ -17,5 +17,9 @@ class CommentsController < ApplicationController
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
+    end
+    
+    def set_article
+      @article = Article.find(params[:article_id])
     end
 end
